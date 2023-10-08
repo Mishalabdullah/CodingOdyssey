a05df9bafdd4ef3d7c12e886aa10c9f67d83a837
Mishalabdullah <github.ijvob@simplelogin.co>
2023-10-08 18:24:51 +0530

    only members app req

--- todoAppMulearn/register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from .import views
-
-urlpatterns = [
-    path("",views.register,name="register"),
-    # path("",views.home,name="home"),
-    # path('todo/', include('todo.urls')),
-     
-]
