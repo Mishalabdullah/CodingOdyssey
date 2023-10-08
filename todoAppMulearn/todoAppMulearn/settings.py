11ee54f8c76da2b763f42870f9fc96ce6bdc18e1
Mishalabdullah <github.ijvob@simplelogin.co>
2023-10-08 17:58:02 +0530

    tweaks

--- todoAppMulearn/todoAppMulearn/settings.py
+++ todoAppMulearn/todoAppMulearn/settings.py
@@ -73,8 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todoAppMulearn.wsgi.application'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
