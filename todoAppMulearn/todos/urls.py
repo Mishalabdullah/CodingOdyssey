661db0ef19f51865b320c63b4cbb3a17de44db2e
Mishalabdullah <github.ijvob@simplelogin.co>
2023-10-09 16:59:41 +0530

    upadint todo's

--- todoAppMulearn/todos/urls.py
+++ todoAppMulearn/todos/urls.py
@@ -4,4 +4,7 @@ from .import views
 urlpatterns = [
     path('',views.todos,name="todos"),
     path('add/', views.add_todo, name="add_todo"),
+    path('done_todo/<int:task_id>/<str:state>/', views.done_todo, name='done_todo'),
+    path('todo_not_done/<int:task_id>/<str:state>/', views.todo_not_done, name='todo_not_done'),
+    
 ]
--- todoAppMulearn/todos/views.py
+++ todoAppMulearn/todos/views.py
@@ -5,10 +5,18 @@ from .forms import TaskForm
 def todos(request):
     # Retrieve tasks for the currently logged-in user
     user = request.user
-    tasks = Task.objects.tasks_for_user(user)
+    tasks = Task.objects.filter(user=user)
 
-    context = {'todos': tasks}
-    return render(request, 'todos.html', context)
+    incomplete_todos = [todo for todo in tasks if not todo.complete and not todo.expired]
+    completed_todos = [todo for todo in tasks if todo.complete]
+    expired_todos = [todo for todo in tasks if todo.expired]
+
+    return render(request, 'todos.html', {
+        'incomplete_todos': incomplete_todos,
+        'completed_todos': completed_todos,
+        'expired_todos': expired_todos,
+    })
+
 
 def add_todo(request):
     if request.method == 'POST':
@@ -19,3 +27,15 @@ def add_todo(request):
     else:
         form = TaskForm()
     return render(request, 'add_todo.html', {'form': form})
+
+def done_todo(request, task_id, state):
+    task = Task.objects.get(pk=task_id)
+    task.complete = not state
+    task.save()
+    return redirect('todos')
+
+def todo_not_done(request, task_id, state):
+    task = Task.objects.get(pk=task_id)
+    task.complete = True
+    task.save()
+    return redirect('todos')
