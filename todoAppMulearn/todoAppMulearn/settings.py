1b8e91047f25e43642c6cfef8c18a69b1c1f7ec1
Mishalabdullah <github.ijvob@simplelogin.co>
2023-10-16 13:55:53 +0530

    testing

--- todoAppMulearn/todoAppMulearn/settings.py
+++ todoAppMulearn/todoAppMulearn/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-!j-^^)me0fbxdme4ef&(e$61wls%nu*8mmui#-@+016smf8kk8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://codingodyssey-production.up.railway.app/']
+ALLOWED_HOSTS = ['codingodyssey-production.up.railway.app']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
