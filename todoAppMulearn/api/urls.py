5e55d390061c45ac9a331190a256c018107a3fd2
Mishalabdullah <github.ijvob@simplelogin.co>
2023-10-16 11:06:53 +0530

    delete todo api done

--- todoAppMulearn/api/urls.py
+++ todoAppMulearn/api/urls.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
   path('', views.api_home, name='api_home'),
   path('add_todo/', views.api_add_todo, name='api_add_todo'),
+  path('delete_todo/<int:todo_id>/', views.api_delete_todo, name='api_delete_todo'),  
   path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- todoAppMulearn/api/views.py
+++ todoAppMulearn/api/views.py
@@ -26,3 +26,20 @@ def api_add_todo(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def api_delete_todo(request, todo_id):
+    try:
+        # Get the todo object by its ID
+        todo = Task.objects.get(pk=todo_id)
+
+        # Check if the todo belongs to the current user
+        if todo.user == request.user:
+            todo.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({'detail': 'You do not have permission to delete this todo.'}, status=status.HTTP_403_FORBIDDEN)
+    except Task.DoesNotExist:
+        return Response({'detail': 'Todo not found.'}, status=status.HTTP_404_NOT_FOUND)
