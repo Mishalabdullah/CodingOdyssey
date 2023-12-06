4cf40b8b20dff2741671e2b08f95cef0ad26cc93
Mishalabdullah <github.ijvob@simplelogin.co>
2023-12-06 16:03:49 +0530

    custom font and

--- adarsh-personal-website/app/components/pagecard.js
+++ adarsh-personal-website/app/components/pagecard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageComponent = ({ pageName, pageLink }) => {
   return (
-    <div>
+    <div className="md:h-full font-inter">
       <a href={pageLink}>
         <div className="bg-[#2525258C] text[#9C9C9C] bg-opacity-40 relative rounded-[52px] m-2 h-48 md:h-full md:w-11/12  p-10">
           {pageName}
