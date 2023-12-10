aece07d9df77e71117a7c619fb7c739b134444e6
Mishalabdullah <github.ijvob@simplelogin.co>
2023-12-10 07:08:25 +0530

    about me page dev

---
+++ adarsh-personal-website/app/components/datacard.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const DataComponent = ({
+  dataName,
+  dataLink,
+  dataText = "",
+  dateStart = "",
+  dateEnd = "",
+}) => {
+  return (
+    <div className="h-64 font-inter">
+      <a href={dataLink}>
+        <div className="bg-[#2525258C] md:text-3xl  text-xl text[#9C9C9C] bg-opacity-40 relative rounded-[52px] m-2 h-full md:w-11/12  p-10">
+          {dataName}
+          <div className="text-[#86868B]">
+            {dataText}
+            <br />
+            {dateStart + dateEnd}
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default DataComponent;
---
+++ adarsh-personal-website/app/components/headingField.js
@@ -0,0 +1,3 @@
+export default function HeadingField({ heading }) {
+  return <p className="text-3xl mt-16">{heading}</p>;
+}
