1f4c2bc7836e1f082adf1465ea2b8feed74b80db
Mishalabdullah <github.ijvob@simplelogin.co>
2023-12-12 15:47:48 +0530

    home page update

--- adarsh-personal-website/app/page.js
+++ adarsh-personal-website/app/page.js
@@ -3,14 +3,14 @@ import PageComponent from "./components/pagecard";
 export default function Home() {
   return (
     <main>
-      <div className="text-[#F5F5F7] text-2xl md:text-5xl font-bold  h-screen relative">
+      <div className="text-[#F5F5F7] overflow-hidden text-2xl md:text-5xl font-bold  h-screen relative">
         <div className="m-2 p-2">
           Hi,<br></br>
           I'm Adarsh, a UX designer based in India. I solve real-world problems
           with empathetic design. I'm seeking fresh design opportunities and
           open to both internships and freelance projects.
         </div>
-        <div className="md:flex md:m-2 md:p-2">
+        <div className="md:flex md:m-2 max-h-screen md:p-2">
           <div className="inline md:flex justify-stretch md:w-6/12">
             <div className="md:w-6/12">
               <PageComponent pageName="Works" pageLink="/works" />
@@ -19,7 +19,7 @@ export default function Home() {
               <PageComponent pageName="About Me" pageLink="/about-me" />
             </div>
           </div>
-          <div className="flex md:flex-col md:w-6/12">
+          <div className="flex md:flex-col justify-around  md:w-6/12">
             <PageComponent pageName="Contact" pageLink="/contact" />
             <div className="md:m-2"></div>
             <PageComponent pageName="Resume" pageLink="/resume" />
