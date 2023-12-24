293b81705f053a351d26e63f99297e7e90585d55
Mishalabdullah <github.ijvob@simplelogin.co>
2023-12-24 10:14:17 +0530

    mobile view componenet

---
+++ adarsh-personal-website/app/components/mobileNavCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const MobileNavComponent = ({ pageName, pageLink }) => {
+  return (
+    <div className="md:h-full text-2xl font-inter">
+      <a href={pageLink}>
+        <div
+          className="bg-[#2525258C] md:hover:bg-[#8FDF92]  inline-block align-bottom  text[#9C9C9C] bg-opacity-40 relative rounded-[52px] m-2 h-36 md:h-full md:w-11/12  md:p-10 p-12"
+          style={{
+            flexGrow: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+          }}
+        >
+          <p className="text-white">{pageName}</p>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default MobileNavComponent;
--- adarsh-personal-website/app/components/mobileNavbar.js
+++ adarsh-personal-website/app/components/mobileNavbar.js
@@ -1,22 +1,22 @@
 "use client";
 import { useState } from "react";
-import PageComponent from "./pagecard";
+import MobileNavComponent from "./mobileNavCard";
 export default function MobileNabar() {
   const [isOpen, setIsOpen] = useState(false);
   const ButtonClick = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <div className="md:hidden  inline-block p-2 z-50  w-10 h-8 fixed bottom-4 right-4">
+    <div className="md:hidden  inline-block p-2 z-50  w-12 h-12 fixed bottom-4 right-4">
       <button onClick={ButtonClick}>
         <img src="./mobileNavbar.png"></img>
       </button>
       {isOpen && (
         <div className="fixed inset-0  bg-[#000] opacity-90 items-center justify-center">
-          <PageComponent pageName="Home" pageLink="/" />
-          <PageComponent pageName="Works" pageLink="/works" />
-          <PageComponent pageName="About Me" pageLink="/about-me" />
-          <PageComponent pageName="Contact" pageLink="/contact" />
+          <MobileNavComponent pageName="Home" pageLink="/" />
+          <MobileNavComponent pageName="Works" pageLink="/works" />
+          <MobileNavComponent pageName="About Me" pageLink="/about-me" />
+          <MobileNavComponent pageName="Contact" pageLink="/contact" />
           <div className="md:hidden inline-block p-2 z-50  w-12 h-12 fixed bottom-4 right-4">
             <button onClick={ButtonClick}>
               <img src="./mobileNavbar.png"></img>
