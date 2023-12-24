c9e71938c25c83f79e82e9269721515b290dbe27
Mishalabdullah <github.ijvob@simplelogin.co>
2023-12-24 09:59:38 +0530

    tweaking opacity

--- adarsh-personal-website/app/components/mobileNavbar.js
+++ adarsh-personal-website/app/components/mobileNavbar.js
@@ -7,12 +7,12 @@ export default function MobileNabar() {
     setIsOpen(!isOpen);
   };
   return (
-    <div className="md:hidden inline-block p-2 z-50  w-12 h-10 fixed bottom-4 right-4">
+    <div className="md:hidden  inline-block p-2 z-50  w-12 h-10 fixed bottom-4 right-4">
       <button onClick={ButtonClick}>
         <img src="./mobileNavbar.png"></img>
       </button>
       {isOpen && (
-        <div className="fixed inset-0  bg-[#2525258C] items-center justify-center">
+        <div className="fixed inset-0  bg-[#000] opacity-90 items-center justify-center">
           <PageComponent pageName="Home" pageLink="/" />
           <PageComponent pageName="Works" pageLink="/works" />
           <PageComponent pageName="About Me" pageLink="/about-me" />
